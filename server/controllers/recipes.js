a6faf3ec5e9c7ce7f76e7b1797c0aab1f2f9bcf5
shiv-chan <kshibuya31@gmail.com>
2021-10-24 23:01:47 -0700

    add controllers

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -1,4 +1,25 @@
 // all handlers for routes
-export const getRecipes = (req, res) => {
-	res.send('This works!');
+import Recipe from '../models/recipeModels.js';
+
+export const getRecipes = async (req, res) => {
+	try {
+		const allRecipes = await Recipe.find();
+		console.log(allRecipes);
+		res.status(200).json(allRecipes);
+	} catch (err) {
+		res.status(404).json({ message: err.message });
+	}
+};
+
+export const createRecipe = async (req, res) => {
+	const recipe = req.body;
+
+	const newRecipe = new Recipe(recipe);
+
+	try {
+		await newRecipe.save();
+		res.status(201).json(newRecipe);
+	} catch (err) {
+		res.status(409).json({ message: err.message });
+	}
 };
