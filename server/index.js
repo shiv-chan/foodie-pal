40a6bd37c79c5696493c91f1b39cc85496058e76
shiv-chan <kshibuya31@gmail.com>
2021-10-27 00:09:02 -0700

    create user model and set up sign up router

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import recipeRoutes from './routes/recipes.js';
+import signUpRoutes from './routes/signUp.js';
 
 const app = express();
 dotenv.config();
@@ -11,6 +12,7 @@ app.use(express.json({ limit: '30mb' }));
 app.use(express.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
 app.use('/recipes', recipeRoutes);
+app.use('/signup', signUpRoutes);
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const PORT = process.env.PORT || 5000;
