36ef7f7d4359e004ddd3fa8e3feef85a625accaf
shiv-chan <kshibuya31@gmail.com>
2021-12-26 12:34:44 -0800

    add the delete account dialog

--- client/src/common/components/Menu.js
+++ client/src/common/components/Menu.js
@@ -1,47 +1,96 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
 	Box,
 	List,
 	SwipeableDrawer,
-	ListItem,
+	ListItemButton,
 	ListItemIcon,
 	ListItemText,
 	Divider,
+	Dialog,
+	DialogTitle,
+	DialogContent,
+	DialogContentText,
+	DialogActions,
+	Button,
 } from '@mui/material';
 import MenuBookRoundedIcon from '@mui/icons-material/MenuBookRounded';
 import AddBoxRoundedIcon from '@mui/icons-material/AddBoxRounded';
 import ReportRoundedIcon from '@mui/icons-material/ReportRounded';
+import { useHistory } from 'react-router-dom';
 
 const Menu = ({ isOpen, setIsOpen }) => {
+	const [isDialogOpen, setIsDialogOpen] = useState(false);
+	const history = useHistory();
+
+	const handleListItemButtonClick = (path) => {
+		history.push(path);
+		setIsOpen(false);
+	};
+
+	const handleClickDeleteAccount = () => {
+		setIsDialogOpen(true);
+	};
+
+	const handleCloseDialog = () => {
+		setIsDialogOpen(false);
+	};
+
 	const list = () => (
 		<Box sx={{ width: 250 }} role="presentation">
-			<List>
-				<ListItem button>
+			<List sx={{ marginBottom: 'auto' }}>
+				<ListItemButton onClick={() => handleListItemButtonClick('/recipes')}>
 					<ListItemIcon>
 						<MenuBookRoundedIcon />
 					</ListItemIcon>
 					<ListItemText primary="Recipes" />
-				</ListItem>
-				<ListItem button>
+				</ListItemButton>
+				<ListItemButton onClick={() => handleListItemButtonClick('/add')}>
 					<ListItemIcon>
 						<AddBoxRoundedIcon />
 					</ListItemIcon>
 					<ListItemText primary="Create Recipe" />
-				</ListItem>
+				</ListItemButton>
 			</List>
 			<Divider />
 			<List>
-				<ListItem button>
+				<ListItemButton onClick={handleClickDeleteAccount}>
 					<ListItemIcon>
 						<ReportRoundedIcon />
 					</ListItemIcon>
 					<ListItemText primary="Delete Account" />
-				</ListItem>
+				</ListItemButton>
 			</List>
+			<Dialog
+				open={isDialogOpen}
+				onClose={handleCloseDialog}
+				aria-labelledby="alert-dialog-title"
+				aria-describedby="alert-dialog-description"
+			>
+				<DialogTitle
+					id="alert-dialog-title"
+					color="error"
+					display="flex"
+					alignItems="center"
+				>
+					<ReportRoundedIcon />
+					{'Delete Account'}
+				</DialogTitle>
+				<DialogContent>
+					<DialogContentText id="alert-dialog-description">
+						Are you sure you want to delete your account? If you delete your
+						account, you will permanently lose your recipes.
+					</DialogContentText>
+				</DialogContent>
+				<DialogActions>
+					<Button onClick={handleCloseDialog} sx={{ color: 'gray' }}>
+						Cancel
+					</Button>
+					<Button color="error">Delete Acccount</Button>
+				</DialogActions>
+			</Dialog>
 		</Box>
 	);
-
-	console.log(isOpen);
 
 	return (
 		<SwipeableDrawer
