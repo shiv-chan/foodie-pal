c61d561bbdfa6e62ac9090a0d7a47cbfd0053f7e
shiv-chan <kshibuya31@gmail.com>
2022-01-19 15:05:31 -0800

    display data of the global state
    replace the context data with the global state's data

--- client/src/feature/recipes/Recipes.js
+++ client/src/feature/recipes/Recipes.js
@@ -1,30 +1,62 @@
-import { Container, Grid } from '@mui/material';
-import React, { useContext, useLayoutEffect } from 'react';
-import { context } from '../../common/context';
+import { Container, Grid, Typography } from '@mui/material';
+import React, { useLayoutEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import RecipeCard from './RecipeCard';
 import { useHistory } from 'react-router-dom';
+import { getAllRecipes } from '../../common/recipesSlice';
 
 const Recipes = () => {
-	const { fakeData } = useContext(context);
 	const history = useHistory();
+	const allRecipes = useSelector((state) => state.recipes.recipes);
+	const status = useSelector((state) => state.recipes.status);
+	const dispatch = useDispatch();
 
 	useLayoutEffect(() => {
 		const email = sessionStorage.getItem('email');
 		!email && history.push('/login');
-	}, [history]);
 
-	return (
-		<Container sx={{ flex: '1 0 auto', my: 5 }}>
-			<h1>This is recipes page. ({sessionStorage.getItem('email')})</h1>
-			<Grid container spacing={2}>
-				{fakeData.map((recipe) => (
-					<Grid item xs={12} md={6}>
-						<RecipeCard recipeData={recipe} />
-					</Grid>
-				))}
-			</Grid>
-		</Container>
-	);
+		dispatch(getAllRecipes({ email }));
+	}, [history, dispatch]);
+
+	if (status === 'idle') {
+		return (
+			<Container sx={{ flex: '1 0 auto', my: 10 }}>
+				<Typography variant="h5" component="p">
+					Loading...
+				</Typography>
+			</Container>
+		);
+	}
+
+	if (status === 'succeeded') {
+		return (
+			<Container sx={{ flex: '1 0 auto', my: 10 }}>
+				<Typography variant="h4" component="h1">
+					Your recipes
+				</Typography>
+				<Typography variant="subtitle1" component="p">
+					Now you are logged in with <b>{sessionStorage.getItem('email')}</b>.
+				</Typography>
+				<Grid container spacing={2}>
+					{allRecipes.map((recipe) => (
+						<Grid item xs={12} md={6} key={recipe._id}>
+							<RecipeCard recipeData={recipe} />
+						</Grid>
+					))}
+				</Grid>
+			</Container>
+		);
+	}
+
+	if (status === 'failed') {
+		return (
+			<Container sx={{ flex: '1 0 auto', my: 10 }}>
+				<Typography variant="h5" component="p">
+					Something went wrong...
+				</Typography>
+			</Container>
+		);
+	}
 };
 
 export default Recipes;
